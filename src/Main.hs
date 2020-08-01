d35306e0189c890ffe141bb19e9f9ffa07b11551
Ubuntu <ubuntu@ip-172-31-25-161.eu-west-2.compute.internal>
2020-08-01 15:36:03 +0000

    added post

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-
 import Web.Scotty
 import Data.Monoid (mconcat)
 import Control.Monad.Trans(liftIO,lift,MonadIO)
@@ -7,12 +6,11 @@ import System.IO
 import Data.Text.Lazy.Encoding (decodeUtf8)
 import Data.Text.Lazy (pack,unpack)
 import Data.Maybe
-
-import Database.MongoDB( PortID( PortNumber ) )
+import Data.Time.Clock.POSIX
 import Database.MongoDB    (Action, Document, Document, Value, access,
-                            allCollections, close, connect, delete, exclude, find,
+                            allCollections, insert_, close, connect, delete, exclude, find,
                             host,findOne, insertMany, master, project, rest,
-                            select, sort, Val, at, (=:))
+                            select, liftDB, sort, Val, at, (=:))
 
 main :: IO ()
 main = scotty 3000 $ do
@@ -20,23 +18,34 @@ main = scotty 3000 $ do
     get "/logs/:id" $ do
 	id <- param "id"
 	log <- liftIO $ selectById $ unpack id
-        --html $ mconcat ["<h1>", pack  $ look "content" $ fromJust log , "</h1>"]
-        --html $ mconcat ["<h1>", pack $ at "content" $ fromJust log , "</h1>"]
-	text $ pack $ at "content" $ fromJust log
+	text $ pack $ getContent log
 
-    post "/readbody" $ do
+    post "/logs" $ do
+        id <- liftIO $ getTimeInMillis
         b <- body
 	let decodedBody = unpack(decodeUtf8 b)
-        liftIO(putStr(decodedBody))
-        liftIO(hFlush stdout)
-        text $ decodeUtf8 b
+	i <- liftIO $ run $ insertLog id decodedBody
+        text $ "Ok"
 
-
-
+--setup database connection
 run::MonadIO m => Action m a -> m a 
 run action = do
         pipe <- liftIO(connect $ host "127.0.0.1")
 	access pipe master "cmus-analytics" action
 
+--Select document by id field, 
 selectById :: (MonadIO m, Val v) => v -> m (Maybe Document)
 selectById id = run $ findOne(select ["id" =: id] "logs")
+
+--get field content from a document (if document does not exist, return emtpy string)
+getContent::Maybe Document -> String
+getContent document = case document of
+	Just x ->  at "content" x
+	Nothing -> ""
+
+--getTimeInMillis
+getTimeInMillis ::Integral b => IO b
+getTimeInMillis = round `fmap` getPOSIXTime
+
+insertLog::MonadIO m => Int -> String -> Action m ()
+insertLog id body = run $ insert_ "logs" ["id" =: id, "content" =: body]
