8ee1391e917296a531d95b0249efcac41b3684e0
Ubuntu <ubuntu@ip-172-31-25-161.eu-west-2.compute.internal>
2020-08-04 22:43:22 +0000

    still to add random

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
+import Data.Digest.Pure.MD5 (md5)
 import Web.Scotty
 import Control.Monad.Trans(liftIO,MonadIO)
+import Control.Monad.Fail
 import System.IO
 import Data.Text.Lazy.Encoding (decodeUtf8)
-import Data.Text.Lazy (pack,unpack)
+import Data.ByteString.Lazy.Char8 as LB
+import Data.Text.Lazy as LT (pack,unpack)
 import Data.Maybe
 import Data.Time.Clock.POSIX
 import Database.MongoDB    (Action, Document, Value, access,
-                            allCollections, insert, connect,
+                            allCollections, upsert, connect,
                             host,findOne, insertMany, master, 
-                            select, Val, at, (=:))
+                            select,aggregate, Val, at, (=:))
 
 main :: IO ()
 main = scotty 3000 $ do
 
     get "/logs/:id" $ do
 	id <- param "id"
-	log <- liftIO $ selectById $ unpack id
-	text $ pack $ getContent log
+	log <- liftIO $ selectById $ LT.unpack id
+	text $ LT.pack $ getContent log
+
+    get "/logs/random" $ do
+	logs <- liftIO $ selectRandom 2
+	text $ LT.pack $ getContent $ Just $ Prelude.head logs
 
     post "/logs" $ do
-        id <- liftIO $ getTimeInMillis
         b <- body
-	let decodedBody = unpack(decodeUtf8 b)
-	i <- liftIO $ insertLog (show id) decodedBody
-        text $ pack $ show id
+	let decodedBody = LT.unpack(decodeUtf8 b)
+        let id = getMD5 decodedBody
+	i <- liftIO $ upsertLog id decodedBody
+        text $ LT.pack $ show id
 
 --setup database connection
 run::MonadIO m => Action m a -> m a 
@@ -36,16 +43,25 @@ run action = do
 selectById :: (MonadIO m, Val v) => v -> m (Maybe Document)
 selectById id = run $ findOne(select ["id" =: id] "logs")
 
+--Select document by id field, 
+selectRandom :: (MonadIO m,MonadFail m) => Integer -> m [Document]
+selectRandom size = run $ aggregate "logs" [["$sample" =: [ "size" =: size ]]]
+
 --get field content from a document (if document does not exist, return emtpy string)
 getContent::Maybe Document -> String
 getContent document = case document of
 	Just x ->  at "content" x
 	Nothing -> ""
 
---getTimeInMillis
-getTimeInMillis ::Integral b => IO b
-getTimeInMillis = round `fmap` getPOSIXTime
+-- Upsert (inserts only if not existing) log with id and content
+upsertLog::MonadIO m => String -> String -> m ()
+upsertLog id body = run $ upsert (select ["id" =: id] "logs") ["id" =: id, "content" =: body]
 
--- Insert log with id and content
-insertLog::MonadIO m => String -> String -> m Value
-insertLog id body = run $ insert "logs" ["id" =: id, "content" =: body]
+--get MD5 of a string representation
+getMD5::String -> String
+getMD5 a = Prelude.map removeQuotes $ show $ md5 $ LB.pack a
+
+--Change double quotes by blank char
+removeQuotes:: Char -> Char
+removeQuotes '"' = ' '
+removeQuotes c = c
